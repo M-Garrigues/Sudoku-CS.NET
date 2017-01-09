31b1c9c3cc64e436792a6cb7bea0d51584b25b97
Mathieu Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2017-01-09 16:16:03 +0100

    Grid functions, constructor and getters

--- ISI Sudoku/ISI Sudoku/Grid.cs
+++ ISI Sudoku/ISI Sudoku/Grid.cs
@@ -6,8 +6,70 @@ using System.Threading.Tasks;
 
 namespace ISI_Sudoku
 {
-    class Grid
+    public class Grid
     {
+        private int[,] tabGrid;
 
-    }
+
+        public Grid()
+        {
+            this.tabGrid = new int[9, 9];
+        }
+
+        public int[,] TabGrid
+        {
+            get
+            {
+                return tabGrid;
+            }
+            set
+            {
+                tabGrid = value;
+            }
+        }
+
+
+
+        public bool checkColumn(int x, int i)
+        {
+            bool test = true;
+            for (int j = 0; j < 9; j++)
+            {
+                if (tabGrid[x,j] == i) test = false;
+            }
+
+            return test;
+        }
+
+
+        public bool checkLine(int y, int i)
+        {
+            bool test = true;
+            for (int j = 0; j < 9; j++)
+            {
+                if (tabGrid[j, y] == i) test = false;
+            }
+
+            return test;
+        }
+
+
+        public bool checkSquare(int x, int y, int i)
+        {
+            bool test = true;
+
+            int xdep = x - x % 3;
+            int ydep = y - y % 3;
+
+            for(int j = 0; j < 3; j++)
+            {
+                for (int k = 0 ; k < 3; k++)
+                {
+                    if (tabGrid[xdep + j, ydep + k] == i) test = false;
+                }
+            }
+            return test;
+        }
+
+    }   
 }
